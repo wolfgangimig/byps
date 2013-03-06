0a9aeb8115ebd7e8a4995b7d960d4ff5ef4db6bd
Wolfgang Imig <wolfgang.imig@googlemail.com>
2013-03-06 22:40:59 +0100

    Added comments

--- java/byps-api/src/com/wilutions/byps/BAsyncResult.java
+++ java/byps-api/src/com/wilutions/byps/BAsyncResult.java
@@ -1,6 +1,16 @@
 package com.wilutions.byps;
 
-
+/**
+ * This is the base class for handling asynchronous results.
+ * @param <T>
+ */
 public interface BAsyncResult<T> {
-	public void setAsyncResult(T obj, Throwable e);
+	
+	/**
+	 * This function is called to set the result.
+	 * Either result or exception is supplied.
+	 * @param result Result object
+	 * @param exception Exception
+	 */
+	public void setAsyncResult(T result, Throwable exception);
 }
