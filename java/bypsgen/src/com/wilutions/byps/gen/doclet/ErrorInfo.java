750e03d3429286abecce2cbefad6eb1d2d5cb88d
Wolfgang Imig <wolfgang.imig@googlemail.com>
2013-07-06 08:55:38 +0200

    Display better error messages

---
+++ java/bypsgen/src/com/wilutions/byps/gen/doclet/ErrorInfo.java
@@ -0,0 +1,47 @@
+package com.wilutions.byps.gen.doclet;
+
+public class ErrorInfo {
+	public String className;
+	public String methodName;
+	public String paramName;
+	public String fieldName;
+	public String typeArg;
+	public String msg;
+	
+	public ErrorInfo() {}
+	
+	
+	public ErrorInfo(ErrorInfo rhs) {
+		this.className = rhs.className;
+		this.methodName = rhs.methodName;
+		this.paramName = rhs.paramName;
+		this.fieldName = rhs.fieldName;
+		this.typeArg = rhs.typeArg;
+		this.msg = rhs.msg;
+	}
+
+	public ErrorInfo copy() { return new ErrorInfo(this); }
+
+	public String toString() {
+		StringBuilder sbuf = new StringBuilder();
+		sbuf.append("[class=");
+		sbuf.append(className);
+		if (methodName != null) {
+			sbuf.append(", method=").append(methodName);
+			if (paramName != null) {
+				sbuf.append(", param=").append(paramName);
+			}
+		}
+		if (fieldName != null) {
+			sbuf.append(", field=").append(fieldName);
+		}
+		if (typeArg != null) {
+			sbuf.append(", arg=").append(typeArg);
+		}
+		if (msg != null) {
+			sbuf.append(": ").append(msg);
+		}
+		sbuf.append("]");
+		return sbuf.toString();
+	}
+}
