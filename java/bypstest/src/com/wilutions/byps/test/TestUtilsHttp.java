844c79d64a61b3a9ac15fccb1bd292fceebcfe49
Wolfgang Imig <wolfgang.imig@googlemail.com>
2013-08-14 11:24:24 +0200

    Removed unused references from classpath files
    Logging, JUNIT, EAR, Tomcat Server

--- java/bypstest/src/com/wilutions/byps/test/TestUtilsHttp.java
+++ java/bypstest/src/com/wilutions/byps/test/TestUtilsHttp.java
@@ -30,8 +30,8 @@ public class TestUtilsHttp {
 
 	private static Log log = LogFactory.getLog(TestUtilsHttp.class);
 	
-	public static String url = "http://localhost:8080/bypstest-srv/bypsservlet";
-	public static String url2 = "http://localhost:6080/bypstest-srv/bypsservlet";
+	public static String url = "http://192.168.0.178:6080/bypstest-srv/bypsservlet";
+	public static String url2 = "http://192.168.0.178:6080/bypstest-srv/bypsservlet";
 	//public static String url = "http://srvtdev02:8020/bypstest-srv/bypsservlet";
 	
 	private static Executor tpool = Executors.newCachedThreadPool();
