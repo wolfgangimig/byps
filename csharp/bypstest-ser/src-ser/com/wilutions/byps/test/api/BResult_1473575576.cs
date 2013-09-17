66f59c4d7ad5a5777cdcdfa8c2509e7a0904f0e2
Wolfgang Imig <wolfgang.imig@googlemail.com>
2013-09-17 22:41:21 +0200

    Javadoc comments

---
+++ csharp/bypstest-ser/src-ser/com/wilutions/byps/test/api/BResult_1473575576.cs
@@ -0,0 +1,19 @@
+﻿//
+// 
+// THIS FILE HAS BEEN GENERATED. DO NOT MODIFY.
+//
+
+using System;
+using System.Collections.Generic;
+using com.wilutions.byps;
+
+namespace com.wilutions.byps.test.api
+{
+	
+	public sealed class BResult_1473575576 : BMethodResult<com.wilutions.byps.test.api.srvr.ClientIF>, BSerializable
+	{
+	
+		
+		public static readonly long serialVersionUID = 1170836883L;		
+	} // end class
+}  // end namespace
---
+++ csharp/bypstest-ser/src-ser/com/wilutions/byps/test/api/BSerializer_1911325389.cs
@@ -0,0 +1,53 @@
+﻿//
+// Serializer for com.wilutions.byps.test.api.BResult_1178379224
+// 
+// THIS FILE HAS BEEN GENERATED. DO NOT MODIFY.
+//
+
+using System;
+using System.Collections.Generic;
+using com.wilutions.byps;
+
+namespace com.wilutions.byps.test.api
+{
+	
+	public class BSerializer_1911325389 : BSerializer {
+		
+		public readonly static BSerializer instance = new BSerializer_1911325389();
+		
+		public BSerializer_1911325389()
+			: base(1911325389) {}
+		
+		public BSerializer_1911325389(int typeId)
+			: base(typeId) {}
+		
+		
+		public override int size(Object obj, BBinaryModel bmodel)
+		{
+			return 0
+			       /* size of base class */ + 0
+			       /* pos=0: result */ + 4			
+			       /* padding up to multiple of alignment */ + 4;			
+		}
+		
+		public override void write(Object obj1, BOutput bout1, long version)
+		{
+			BResult_1178379224 obj = (BResult_1178379224)obj1;			
+			BOutputBin bout = (BOutputBin)bout1;
+			BBufferBin bbuf = bout.bbuf;
+			bout.writeObj(obj._result, false, null);
+		}
+		
+		public override Object read(Object obj1, BInput bin1, long version)
+		{
+			BInputBin bin = (BInputBin)bin1;
+			BResult_1178379224 obj = (BResult_1178379224)(obj1 != null ? obj1 : bin.onObjectCreated(new BResult_1178379224()));
+			
+			BBufferBin bbuf = bin.bbuf;
+			obj._result = (com.wilutions.byps.test.api.ver.EvolveIF)bin.readObj(false, null);
+			
+			return obj;
+		}
+		
+	}
+} // namespace
