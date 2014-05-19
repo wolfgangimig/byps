5dc79ac5148357c0c15b2a240c6114d8f89f698f
Imig <w.imig@elo.com>
2014-05-19 11:16:35 +0200

    BNegotiate must take negotiated bversion into account.

--- java/byps/src/byps/BNegotiate.java
+++ java/byps/src/byps/BNegotiate.java
@@ -115,7 +115,7 @@ public class BNegotiate {
 		}
 
 		final BTargetId tid = (targetId != null ? targetId : BTargetId.ZERO);
-		bbuf.putString(tid.makeSerializeString(BMessageHeader.BYPS_VERSION_CURRENT));
+		bbuf.putString(tid.makeSerializeString(bversion));
 		
 		if (bversion != 0) {
 		  bbuf.putInt(bversion);
