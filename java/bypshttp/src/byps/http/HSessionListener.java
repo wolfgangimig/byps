863f30b7d92eb6346643d36e20eb3104deaf3896
Imig <w.imig@elo.com>
2015-05-08 12:08:57 +0200

    HSessionListener has to implement HttpSessionListener in order to run under Wildfly.

--- java/bypshttp/src/byps/http/HSessionListener.java
+++ java/bypshttp/src/byps/http/HSessionListener.java
@@ -1,11 +1,13 @@
 package byps.http;
 /* USE THIS FILE ACCORDING TO THE COPYRIGHT RULES IN LICENSE.TXT WHICH IS PART OF THE SOURCE CODE PACKAGE */
 import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 
 import byps.BHashMap;
 
 @WebListener
-public class HSessionListener {
+public class HSessionListener implements HttpSessionListener {
 
   //private final static Log log = LogFactory.getLog(HSessionListener.class);
   private final static BHashMap<String, HSession> sessions = new BHashMap<String, HSession>();
@@ -16,5 +18,13 @@ public class HSessionListener {
 
   public static BHashMap<String, HSession> getAllSessions() {
     return sessions;
+  }
+
+  @Override
+  public void sessionCreated(HttpSessionEvent arg0) {
+  }
+
+  @Override
+  public void sessionDestroyed(HttpSessionEvent arg0) {
   }
 }
