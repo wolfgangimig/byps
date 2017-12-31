0ba510a1f6898ef37b5a746c7b9443fb095f5087
wolfgangimig <wolfgangimig@192.168.0.38>
2017-12-31 11:07:49 +0100

    Make cpp compilable on MacOS

---
+++ cpp/common/bypshttp/include/platform/mac/MacHttpClient.h
@@ -0,0 +1,17 @@
+/* USE THIS FILE ACCORDING TO THE COPYRIGHT RULES IN LICENSE.TXT WHICH IS PART OF THE SOURCE CODE PACKAGE */
+#ifndef MAC_HTTPCLIENT_H_
+#define MAC_HTTPCLIENT_H_
+
+
+#include "Byps.h"
+#include "Bypshttp.h"
+
+namespace byps { namespace http {
+    
+    PHttpClient HttpClient_create(void* app);
+    
+}}
+
+
+
+#endif
---
+++ cpp/common/bypshttp/include/platform/mac/MacHttpClient.hpp
@@ -0,0 +1,11 @@
+#include "MacHttpClient.h"
+
+namespace byps { namespace http {
+    
+    PHttpClient HttpClient_create(void* app) {
+        return PHttpClient();
+    }
+    
+}}
+
+
