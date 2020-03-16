d8b5e9c4b832be77c28e7f0bed1a2f122e3b2d9a
Wolfgang Imig <w.imig@elo.com>
2020-03-16 14:13:29 +0100

    BYPS-18: OAuth/SAML support for ELO Indexserver

--- csharp/bypshttp/HConstants.cs
+++ csharp/bypshttp/HConstants.cs
@@ -6,19 +6,26 @@ using System.Text;
 
 namespace byps
 {
-    public class HConstants
-    {
+  public class HConstants
+  {
 
-        public readonly static int DEFAULT_BYTE_BUFFER_SIZE = 10 * 1000;
-	    public readonly static long REQUEST_TIMEOUT_MILLIS = 600 * 1000L;
-	    public readonly static long CLEANUP_MILLIS = 10 * 1000;
-	    public readonly static long MAX_WAIT_FOR_LONGPOLL_MILLIS = 30 * 1000L; 
-	    public readonly static long KEEP_MESSAGE_AFTER_FINISHED = 10L * 1000L;
-	    public readonly static bool PROCESS_PUT_STREAM_ASYNC = false;
-	    public readonly static bool PROCESS_LONGPOLL_ASYNC = true;
-	    public readonly static bool PROCESS_MESSAGE_ASYNC = false;
-	    public readonly static int INCOMING_STREAM_BUFFER = 1000 * 1000;
-	    public readonly static String DEFAULT_CONTENT_TYPE = "application/octet-stream";
+    public readonly static int DEFAULT_BYTE_BUFFER_SIZE = 10 * 1000;
+    public readonly static long REQUEST_TIMEOUT_MILLIS = 600 * 1000L;
+    public readonly static long CLEANUP_MILLIS = 10 * 1000;
+    public readonly static long MAX_WAIT_FOR_LONGPOLL_MILLIS = 30 * 1000L;
+    public readonly static long KEEP_MESSAGE_AFTER_FINISHED = 10L * 1000L;
+    public readonly static bool PROCESS_PUT_STREAM_ASYNC = false;
+    public readonly static bool PROCESS_LONGPOLL_ASYNC = true;
+    public readonly static bool PROCESS_MESSAGE_ASYNC = false;
+    public readonly static int INCOMING_STREAM_BUFFER = 1000 * 1000;
+    public readonly static String DEFAULT_CONTENT_TYPE = "application/octet-stream";
 
-    }
+    /// <summary>
+    /// HTTP session cookie name.
+    /// </summary>
+    /// <remarks>
+    /// BYPS-18. Use this name as parameter in function HWireClient.getHttpCookie
+    /// </remarks>
+    public readonly static String HTTP_COOKIE_JSESSIONID = "JSESSIONID";
+  }
 }
