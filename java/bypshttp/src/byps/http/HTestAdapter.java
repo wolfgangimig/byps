b626a74cccbadf3fef8b0e067b6f143165a51b3a
Wolfgang Imig <w.imig@elo.com>
2020-11-24 20:33:29 +0100

    BYPS-33: Nachzügler eincheckn

--- java/bypshttp/src/byps/http/HTestAdapter.java
+++ java/bypshttp/src/byps/http/HTestAdapter.java
@@ -115,10 +115,4 @@ public class HTestAdapter implements BTestAdapter {
 	 * 
 	 */
   public static final String TIMEOUT_FOR_PROCESSING = "timeout-for-processing";
-
-  /**
-   * Invalidate the HTTP session cookie.
-   * BYPS-33
-   */
-  public static final Object INVALIDATE_HTTP_SESSION = "invalidate-http-session";
 }
